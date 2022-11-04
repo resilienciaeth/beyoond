9ac07392f17edcdd1c5370def014dc2f38c7233c
resilienciaeth <106503220+resilienciaeth@users.noreply.github.com>
2022-11-04 14:11:07 -0400

    footer-q6

--- components/q6.jsx
+++ components/q6.jsx
@@ -27,7 +27,7 @@ const {t} = useTranslation();
   return (
     <div className="border-b-[1px] border-t-black border-black w-full py-3 mt-2 space-x-4">
             <div className="flex justify-between ml-4 w-[85%]" onClick={() => toggle(i)}>
-              <h2>{t('q5')}</h2>
+              <h2>{t('q6')}</h2>
               <span className="">{selected === i ? <img src="/assets/utilitymenu2.png" width={20} height={20} alt="arrow" /> : <img src="/assets/utilitymenu.png" width={20} height={20} />}</span>
             </div>
             <div className={selected === i ? 'content show' : 'content'}>{t('a6')}</div>
--- pages/FAQ.js
+++ pages/FAQ.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation, UseTranslation } from 'next-i18next';
 import Image from 'next/image'
+import { Link } from 'react-scroll';
 
 
 import images from '../public/assets'
@@ -24,7 +25,7 @@ const FAQ = () => {
             <Image src={images.faqbanner} objectFit='cover' />
         </div>
         <Faq1 />
-        <div className="h-[60vh] mt-20 bg-black flex flex-col items-center justify-between">
+        <div className=" mt-20 bg-black flex flex-col items-center justify-between">
         <div className="mt-20">
           <div className="flex items-center justify-center">
             <Image src={images.logofooter} />
@@ -32,37 +33,27 @@ const FAQ = () => {
           <div className="flex flex-row mt-10 justify-between w-full space-x-16">
             <div className="">
               <div>
-                <p className="text-beyond-1 text-[8px]">Learn</p>
+                <p className="text-beyond-1 text-[8px] nm:text-[20px]">{t('Join_')}</p>
               </div>
               <div className="space-y-2 mt-4">
-                <p className="text-white text-left text-[12px] font-bold">Create</p>
-                <p className="text-white text-[12px] font-bold">Collect</p>
-                <p className="text-white text-[12px] font-bold">Sell</p>
+                <Link to='test-1' spy={true} smooth={true} offset={50} duration={500} className="text-white hover:text-gray-600 text-left text-[12px] nm:text-[20px] font-bold cursor-pointer">{t('Learn')}</Link>
+                <p onClick={() => window.open('https://forms.gle/GUQeAoUAQ7qA2ybQ7')} className="text-white hover:text-gray-600 text-[12px] nm:text-[20px] font-bold cursor-pointer">{t('navbar_button')}</p>
+                <p onClick={() => window.open('https://www.spatial.io/s/AXK4N-Morning-Naked-Meta-63626a426ce24900014b9c86?share=2965428990700775752')} className="text-white text-[12px] nm:text-[20px] font-bold hover:text-gray-600 cursor-pointer">{t('button4')}</p>
               </div>
             </div>
             <div className="">
               <div>
-                <p className="text-beyond-1 text-[8px]">Company</p>
+                <p className="text-beyond-1 text-[8px] nm:text-[20px]">{t('Connect_')}</p>
               </div>
               <div className="space-y-2 mt-4">
-                <p className="text-white text-[12px] font-bold">Careers</p>
-                <p className="text-white text-[12px] font-bold">Help Center</p>
-                <p className="text-white text-[12px] font-bold">Subscribe</p>
-              </div>
-            </div>
-            <div className="">
-              <div>
-                <p className="text-beyond-1 text-[8px]">Connect</p>
-              </div>
-              <div className="space-y-2 mt-4">
-                <p className="text-white text-[12px] font-bold">Twitter</p>
-                <p className="text-white text-[12px] font-bold">Instagram</p>
-                <p className="text-white text-[12px] font-bold">Discord</p>
+                <Link onClick={() => window.open('https://www.instagram.com/beyoond_io/')} className="text-white text-left text-[12px] nm:text-[20px] font-bold hover:text-gray-600 cursor-pointer">Twitter</Link>
+                <p onClick={() => window.open('https://www.instagram.com/beyoond_io/')}  className="text-white text-[12px] hover:text-gray-600 nm:text-[20px] font-bold cursor-pointer">Instagram</p>
+                <p onClick={() => window.open('https://discord.gg/k37Z8AvCFk')}  className="text-white text-[12px] nm:text-[20px] hover:text-gray-600 font-bold cursor-pointer">Discord</p>
               </div>
             </div>
           </div>
-          <div className="flex text-white mt-20 text-[8px] text-center items-center justify-center">
-            <p>Powered by Naked Meta & Rojkind Arquitectos.</p>
+          <div className="flex text-white mt-20 mb-20 text-[8px] nm:text-[20px] text-center items-center justify-center">
+            <p>Powered by <span className='cursor-pointer hover:text-gray-600 font-bold' onClick={() => window.open('https://nakedmeta.io')}>Naked Meta</span> & <span className='cursor-pointer font-bold hover:text-gray-600' onClick={() => window.open('https://rojkindarquitectos.com/')}>Rojkind Arquitectos.</span> </p>
           </div>
         </div>
       </div>
