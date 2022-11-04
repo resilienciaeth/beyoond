4abeb4fca09996430b5268dceb2ecb646d1a0171
resilienciaeth <106503220+resilienciaeth@users.noreply.github.com>
2022-11-04 13:44:41 -0400

    corrections

--- pages/index.js
+++ pages/index.js
@@ -149,7 +149,7 @@ function Home() {
             <p className="text-[10px] nm:text-[15px] mt-4 nm:mt-10">{t('price')}</p>
             <p className="text-[35px] nm:text-[50px] font-bold">$100 USD</p>
             <div className="flex flex-col mt-4 mb-6 nm:mb-10">
-              <p className="text-[8px] text-center">{t('presale')}</p>
+              <p className="text-[8px] nm:text-[15px] text-center">{t('presale')}</p>
               <div className="flex flex-row items-center justify-between space-x-16 mt-4 nm:mt-10">
               <h1 className='text-[30px] font-bold'>
                 COMING SOON
@@ -358,7 +358,7 @@ function Home() {
               <Image onClick={() => window.open('https://www.instagram.com/kamuleon/')} src={images.jose} />
             </div>
             <div className='cursor-pointer'>
-              <Image onClick={() => window.open('https://www.instagram.com//')} src={images.kevin} />
+              <Image onClick={() => window.open('https://www.instagram.com/kev_prada/ ')} src={images.kevin} />
             </div>
           </div>
           {/*meet the team mobile */}
@@ -397,7 +397,7 @@ function Home() {
             </div>
             <div className='min-w-[160px] space-x-4 flex mt-8 flex-row'>
             <div>
-              <Image src={images.kevin} />
+              <Image onClick={() => window.open('https://www.instagram.com/kev_prada/ ')} src={images.kevin} />
               </div>
               <div>
               <Image src={images.kevin1} />
@@ -479,9 +479,9 @@ function Home() {
                 <p className="text-beyond-1 text-[8px] nm:text-[20px]">{t('Join_')}</p>
               </div>
               <div className="space-y-2 mt-4">
-                <Link to='test-1' spy={true} smooth={true} offset={50} duration={500} className="text-white text-left text-[12px] nm:text-[20px] font-bold cursor-pointer">{t('Learn')}</Link>
-                <p onClick={() => window.open('https://forms.gle/GUQeAoUAQ7qA2ybQ7')} className="text-white text-[12px] nm:text-[20px] font-bold cursor-pointer">{t('navbar_button')}</p>
-                <p onClick={() => window.open('https://www.spatial.io/s/AXK4N-Morning-Naked-Meta-63626a426ce24900014b9c86?share=2965428990700775752')} className="text-white text-[12px] nm:text-[20px] font-bold cursor-pointer">{t('button4')}</p>
+                <Link to='test-1' spy={true} smooth={true} offset={50} duration={500} className="text-white hover:text-gray-600 text-left text-[12px] nm:text-[20px] font-bold cursor-pointer">{t('Learn')}</Link>
+                <p onClick={() => window.open('https://forms.gle/GUQeAoUAQ7qA2ybQ7')} className="text-white hover:text-gray-600 text-[12px] nm:text-[20px] font-bold cursor-pointer">{t('navbar_button')}</p>
+                <p onClick={() => window.open('https://www.spatial.io/s/AXK4N-Morning-Naked-Meta-63626a426ce24900014b9c86?share=2965428990700775752')} className="text-white text-[12px] nm:text-[20px] font-bold hover:text-gray-600 cursor-pointer">{t('button4')}</p>
               </div>
             </div>
             <div className="">
@@ -489,14 +489,14 @@ function Home() {
                 <p className="text-beyond-1 text-[8px] nm:text-[20px]">{t('Connect_')}</p>
               </div>
               <div className="space-y-2 mt-4">
-                <Link onClick={() => window.open('https://www.instagram.com/beyoond_io/')} className="text-white text-left text-[12px] nm:text-[20px] font-bold cursor-pointer">Twitter</Link>
-                <p onClick={() => window.open('https://www.instagram.com/beyoond_io/')}  className="text-white text-[12px] nm:text-[20px] font-bold cursor-pointer">Instagram</p>
-                <p onClick={() => window.open('https://discord.gg/k37Z8AvCFk')}  className="text-white text-[12px] nm:text-[20px] font-bold cursor-pointer">Discord</p>
+                <Link onClick={() => window.open('https://www.instagram.com/beyoond_io/')} className="text-white text-left text-[12px] nm:text-[20px] font-bold hover:text-gray-600 cursor-pointer">Twitter</Link>
+                <p onClick={() => window.open('https://www.instagram.com/beyoond_io/')}  className="text-white text-[12px] hover:text-gray-600 nm:text-[20px] font-bold cursor-pointer">Instagram</p>
+                <p onClick={() => window.open('https://discord.gg/k37Z8AvCFk')}  className="text-white text-[12px] nm:text-[20px] hover:text-gray-600 font-bold cursor-pointer">Discord</p>
               </div>
             </div>
           </div>
           <div className="flex text-white mt-20 mb-20 text-[8px] nm:text-[20px] text-center items-center justify-center">
-            <p>Powered by Naked Meta & Rojkind Arquitectos.</p>
+            <p>Powered by <span className='cursor-pointer hover:text-gray-600 font-bold' onClick={() => window.open('https://nakedmeta.io')}>Naked Meta</span> & <span className='cursor-pointer font-bold hover:text-gray-600' onClick={() => window.open('https://rojkindarquitectos.com/')}>Rojkind Arquitectos.</span> </p>
           </div>
         </div>
       </div>
